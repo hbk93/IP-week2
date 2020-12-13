6937af6cc8aabab72fb864a6155515af0640ab6d
Harry Bett <harrybett89@gmail.com>
2020-12-13 14:08:50 +0300

    create getAkanName function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 var akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+function getAkanName() {
+  var currentDate = new Date();
+  var date = document.getElementById("date").value;
+  var gender = document.getElementsByName("gender").value;
+  var d = date.getDay();
+  var month = date.getMonth();
+  var year = date.getYear();
+  var day = date.getDate();
+  if (date > currentDate) {
+    alert("You couldn't possibly have been born in the future. Enter valid Date.");
+  }
+  else if (month < 0 || month > 11) {
+    alert("Enter valid Month.");
+  }
+  else if (day < 1 || day > 31) {
+    alert("Enter valid Day")
+  }
+  else if (gender === "Male") {
+    alert("Your Akan name is " + akanMaleName[d])
+  }
+  else if (gender === "Female") {
+    alert("Your Akan name is " + akanFemaleName[d])
+  }
+}
+
+
