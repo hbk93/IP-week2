555ef97fb4f8900c5a7e1d461d6b07d55a374719
Harry Bett <harrybett89@gmail.com>
2020-12-20 13:24:03 +0300

    add calculateBithday function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
-var akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-var akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+let akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let akanfemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function getAkanName() {
-  var currentDate = new Date();
-  var date = document.getElementById("date").value;
-  var gender = document.getElementsByName("gender").value;
-  var d = date.getDay();
-  var month = date.getMonth();
-  var year = date.getYear();
-  var day = date.getDate();
-  if (date > currentDate) {
-    alert("Enter valid Date.");
+
+function calculateBirthday() {
+  let date = parseInt(document.getElementById('day').value);
+  let month = parseInt(document.getElementById('month').value);
+  let year = document.getElementById('year').value;
+  let gender = document.getElementById('gender').value;
+  let CC = year.slice(0, 2);
+  let YY = year.slice(2, 4);
+ 
+  if (date === "" || date < 0 || date > 31) {
+    alert ("Enter valid date")
   }
-  else if (month < 0 || month > 11) {
-    alert("Enter valid Month.");
+  else if (month === "" || month < 1 || month > 12) {
+    alert ("Enter valid month")
   }
-  else if (day < 1 || day > 31) {
-    alert("Enter valid Day")
-  }
-  else if (gender === "Male") {
-    alert("Your Akan name is " + akanMaleName[d])
-  }
-  else if (gender === "Female") {
-    alert("Your Akan name is " + akanFemaleName[d])
+  else if (year === "" || year < 1900 || year.length > 4) {
+    alert ("Enter valid year")
   }
   else {
-    alert("Do fill in the blanks");
+    var days = Math.floor((((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + ((26 * (month + 1) / 10)) + date) % 7);
+  }
+
+
+  if (gender === 'male') {
+    document.getElementById('yourName').innerHTML = ("Your name is " + akanMaleName[days] + " and you were born on " + daysOfTheWeek[days]);
+
+  } else if (gender === 'female') {
+    document.getElementById('yourName').innerHTML = ("Your name is " + akanfemaleName[days-1] + " and you were born on " + daysOfTheWeek[days]);
+
+  } else {
+    alert("Please make sure you've choosen your gender!!");
   }
 }
-
-
